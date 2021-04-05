94cb4f26d1f74f56a2183fdf219af30695349f2b
amanuel <amanueldemelash12@gmail.com>
2021-04-04 17:59:53 -0700

    First Commit

---
+++ Myride/app/src/main/java/com/aman/myride/ui/home/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.aman.myride.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.aman.myride.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        homeViewModel =
+                ViewModelProvider(this).get(HomeViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_home, container, false)
+
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+
+        })
+        return root
+    }
+}
